6689ab3b44ca9654763527502db5a0f1c0bef727
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-09-02 14:38:22 +0200

    Assert that products belong to many tags, and tags belong to many products

--- models/index.js
+++ models/index.js
@@ -15,8 +15,21 @@ Category.hasMany(Product, {
 });
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  }
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  }
+})
+
 
 module.exports = {
   Product,
