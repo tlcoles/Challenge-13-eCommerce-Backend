53e4fce752100c3827fef57f9cb7243de92ff296
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-09-03 16:09:23 +0200

    Add a category create route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -33,8 +33,17 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const newCategory = await Category.create({
+      id: req.body.id,
+      category_name: req.body.category_name,
+    })
+    res.status(200).json(newCategory);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 router.put('/:id', (req, res) => {
