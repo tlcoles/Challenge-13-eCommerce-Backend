2b0a9a9ddba8be9b3771dbab51d7671627e11733
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-09-03 16:29:12 +0200

    Add route for deletedCategory

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -64,8 +64,22 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const deletedCategory = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!deletedCategory) {
+      res.status(404).json({ message: 'Sorry, no category with this ID!' });
+      return;
+  }
+  res.status(200).json(deletedCategory);    
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
