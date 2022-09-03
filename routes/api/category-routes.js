48a818d817506335974286b9a9a481c044f9d5cc
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-09-03 16:03:27 +0200

    Add a category-by-ID route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -16,9 +16,21 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryById = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    if (!categoryById) {
+      res.status(404).json({ message: 'Sorry, we cannot find a category with that ID.' });
+      return;
+    }
+    res.status(200).json(categoryById);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 router.post('/', (req, res) => {
