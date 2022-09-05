3be6a019001a0232e332bb5cfd1fc40b15961654
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-09-05 11:35:26 +0200

    Change error message for specificity

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const apiRoutes = require('./api');
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
-  res.send("<h1>Wrong route. Please try again!</h1>")
+  res.send("<h1>Wrong route. Specify the /api routes!</h1>")
 });
 
 module.exports = router;
